d29ffcf74729cdaa1a66199e7399d05a4329b6d5
hkitty <hkitty1488@gmail.com>
2017-01-17 13:17:18 +0200

    args is work

--- DNSclient/src/Main.java
+++ DNSclient/src/Main.java
@@ -5,8 +5,7 @@ public class Main {
 	public static void main(String[] args) { 
 		if (args.length > 0) { 
 			try { 
-				//InetAddress addrArr = InetAddress.getByName("www.vk.com"); 
-				InetAddress[] addrArr = InetAddress.getAllByName("vk.com");
+				InetAddress[] addrArr = InetAddress.getAllByName(args[0]);
 				for (int i = 0; i < addrArr.length; ++i) {
 					System.out.println(addrArr[i]); 
 				}
