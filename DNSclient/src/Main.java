2d3dc4dce559beef0bd6ae8f0b5b0725dbc1f064
hkitty <hkitty1488@gmail.com>
2017-01-17 13:13:23 +0200

    used getAllByName

--- DNSclient/src/Main.java
+++ DNSclient/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 	public static void main(String[] args) { 
 		if (args.length > 0) { 
 			try { 
-				InetAddress addrArr = InetAddress.getByName("www.vk.com"); 
-				//InetAddress[] addrArr = "vk.com";
-				//for (int i = 0; i < addrArr.length; ++i) {
-					System.out.println(addrArr); 
-				//}
+				//InetAddress addrArr = InetAddress.getByName("www.vk.com"); 
+				java.net.InetAddress[] addrArr = java.net.InetAddress.getAllByName("vk.com");
+				for (int i = 0; i < addrArr.length; ++i) {
+					System.out.println(addrArr[i]); 
+				}
 			}
 			catch (UnknownHostException e) { 
 				e.printStackTrace();
