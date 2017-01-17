27a0517a3e90057cfe3c9d9aaa0b72a53a5e1f45
hkitty <hkitty1488@gmail.com>
2017-01-17 13:15:37 +0200

    import fixed

--- DNSclient/src/Main.java
+++ DNSclient/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		if (args.length > 0) { 
 			try { 
 				//InetAddress addrArr = InetAddress.getByName("www.vk.com"); 
-				java.net.InetAddress[] addrArr = java.net.InetAddress.getAllByName("vk.com");
+				InetAddress[] addrArr = InetAddress.getAllByName("vk.com");
 				for (int i = 0; i < addrArr.length; ++i) {
 					System.out.println(addrArr[i]); 
 				}
